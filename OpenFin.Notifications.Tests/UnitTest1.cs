559f842167ca1a7883f3e843d729b0564714c7c3
lesjas <48453267+lesjas@users.noreply.github.com>
2019-11-22 15:07:15 -0500

    initial commit

---
+++ OpenFin.Notifications.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenFin.Notifications.Tests
+{
+    [TestClass]
+    public class NotificationClientTests
+    {
+        [TestMethod]
+        public void Initialize_NullInitCompleteHandler_ExceptionThrown()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => { NotificationClient.Initialize(); });
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            
+            NotificationClient.OnInitComplete += () =>
+            {
+                NotificationClient.CreateNotificationAsync()
+            };
+
+            NotificationClient.Initialize();
+        }
+    }
+}
